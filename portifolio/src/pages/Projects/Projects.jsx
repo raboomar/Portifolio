1a2b338088227d806b067b11d9ea35f974135f53
Rami <aboomar.2@osu.edu>
2022-09-08 19:23:29 -0400

    added project

--- portifolio/src/pages/Projects/Projects.jsx
+++ portifolio/src/pages/Projects/Projects.jsx
@@ -10,6 +10,35 @@ function Projects() {
         <div className="project1">
           <div className=" project1-box">
             <div>
+              <h3>My Library</h3>
+              <p>
+                <strong> Link:</strong>
+                <a href="https://ramibooklibrary.herokuapp.com/">My Library</a>
+              </p>
+              <p>
+                <strong>Description: </strong> A book collection manager, where
+                you can add, remove, and update books.
+                <p>
+                  Users are required to have an account. if you would like to
+                  check it out please use this login or register
+                  <br /> email: test1@yahoo.com password: test12345
+                </p>
+              </p>
+              <p>
+                <strong> Technologies: </strong>
+                <br />
+                Front-end: React.js. Pure CSS Redux Toolkit
+                <br />
+                Back-end: Express.js, MongoDB. <br />
+                Deployment: Heroku.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="project2">
+          <div className=" project1-box">
+            <div>
               <h3>Admin Dashboard</h3>
               <p>
                 <strong> Link:</strong>
@@ -33,7 +62,7 @@ function Projects() {
           </div>
         </div>
 
-        <div className="project2">
+        <div className="project3">
           <div className="project2-box ">
             <div>
               <h3>Rock Paper Scissors</h3>
@@ -54,6 +83,32 @@ function Projects() {
                 Front-end: HTML, CSS, Javascript. <br />
                 Deployment: Github pages.
               </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="project4">
+          <div className=" project1-box">
+            <div>
+              <h3>Coming Soon</h3>
+              {/* <p>
+                <strong> Link:</strong>
+                <a href="https://main.d21nudikvh3ule.amplifyapp.com/">
+                  User Dashboard
+                </a>
+              </p>
+              <p>
+                <strong>Description: </strong> This is a full stack website that
+                allows users <br /> to cerate, read, update, and delete (CRUD)
+                users as they please.
+              </p>
+              <p>
+                <strong> Technologies: </strong>
+                <br />
+                Front-end: React.js, antd-design. <br />
+                Back-end: Spring Boot API, Spring JPA. <br />
+                Deployment: AWS RDS, Elastic BeanStalk, Amplify, and Route 53.
+              </p> */}
             </div>
           </div>
         </div>
