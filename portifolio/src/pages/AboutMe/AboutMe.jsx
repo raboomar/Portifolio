6c856f74cc64874358be20d9989cb7f41245a6e9
Rami <aboomar.2@osu.edu>
2022-09-17 11:35:10 -0400

    updated about me

--- portifolio/src/pages/AboutMe/AboutMe.jsx
+++ portifolio/src/pages/AboutMe/AboutMe.jsx
@@ -8,15 +8,17 @@ const AboutMe = () => {
         <h2>About me</h2>
         <p className="intro">
           I was first introduced to computing as an undergraduate. I was amazed
-          by their logic, functions, and the manipulation and understanding of
+          by the logic, functions, and ability to manipulate and understand
           data. I quickly realized how powerful Microsoft is and took it upon
           myself to understand and learn Microsoft Excel; from there, I built
           different macros to help automate simple tasks at work using VBA and
           macros. I soon realized that there is a whole world of programming
           languages. For the past two years, I have been learning new
           programming languages via udemy.com. Learning Java, HTML, CSS, and
-          JavaScript has developed into a personal hobby and now a full time
-          career i'm currently a junior developer at TEKSystems Global
+          JavaScript has developed into a personal hobby and is now a full-time
+          career. I'm a junior developer at TEKSystems Global using Java,
+          JavaScript and React to help clients achieve their goals by building
+          solutions to thrive in a new world of opportunities.
         </p>
       </div>
     </div>
